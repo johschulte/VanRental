ddd88a245e0a2bb0c899749d462fc548b6a52fd6
John Schulte <johannes-schulte@gmx.de>
2024-01-22 20:22:35 +0100

    Log In with user name and password - implemented missing Host Sections

---
+++ src/components/AuthRequired.jsx
@@ -0,0 +1,19 @@
+import { Outlet, Navigate, useOutletContext } from "react-router-dom";
+
+const AuthRequired = () => {
+  const { authenticated } = useOutletContext();
+
+  const isLoggedIn = localStorage.getItem("loggedin");
+
+  if (authenticated || Boolean(isLoggedIn)) {
+    return <Outlet />;
+  }
+
+  return (
+    <>
+      <Navigate to="login" state={{ failedRouting: true }} />;
+    </>
+  );
+};
+
+export default AuthRequired;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,39 @@
 import { useState } from "react";
+import { useOutletContext, useNavigate, useLocation } from "react-router-dom";
+import { loginUser } from "../api";
 
 const Login = () => {
-  const [loginFormData, setLoginFormData] = useState({
+  const [loginFormData, setLoginFormData, handleCheckbox] = useState({
     email: "",
     password: "",
   });
+  const { setAuthenticated, authenticated } = useOutletContext();
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
 
-  const handleSubmit = (e) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const { state } = useLocation();
+
+  const from = location.state?.from || "/host";
+
+  function handleSubmit(e) {
     e.preventDefault();
-    console.log(loginFormData);
-  };
+    setStatus("submitting");
+    loginUser(loginFormData)
+      .then((data) => {
+        setError(null);
+        localStorage.setItem("loggedin", true);
+        navigate(from, { replace: true });
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setStatus("idle");
+      });
+  }
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -20,26 +44,50 @@ const Login = () => {
   };
 
   return (
-    <div className="login-container">
-      <h1>Sign in to your account</h1>
-      <form onSubmit={handleSubmit} className="login-form">
-        <input
-          name="email"
-          onChange={handleChange}
-          type="email"
-          placeholder="Email address"
-          value={loginFormData.email}
-        />
-        <input
-          name="password"
-          onChange={handleChange}
-          type="password"
-          placeholder="Password"
-          value={loginFormData.password}
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <>
+      <div className="login-container">
+        {state?.failedRouting && (
+          <h3 className="login-first">
+            You must logged in, in order to visit the "Host" page.
+          </h3>
+        )}
+        <h1>Sign in to your account</h1>
+        <p>Try: Joh@nnes.com as Email and p123 as password.</p>
+        <form onSubmit={handleSubmit} className="login-form">
+          <input
+            name="email"
+            onChange={handleChange}
+            type="email"
+            placeholder="Email address"
+            value={loginFormData.email}
+          />
+          <input
+            name="password"
+            onChange={handleChange}
+            type="password"
+            placeholder="Password"
+            value={loginFormData.password}
+          />
+          <button>Log in</button>
+          <div className="login-switch">
+            <p>Switch login state:</p>
+            <input
+              type="checkbox"
+              id="checkbox"
+              checked={authenticated}
+              onChange={() => setAuthenticated((prev) => !prev)}
+            />
+            <label htmlFor="checkbox"></label>
+          </div>
+          {authenticated && (
+            <>
+              <p>You are now logged in! </p>
+              <p>Try again to visit the "Host" section.</p>
+            </>
+          )}
+        </form>
+      </div>
+    </>
   );
 };
 
